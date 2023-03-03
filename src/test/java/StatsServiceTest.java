5d16207d6c38b791519890d84f3a42e802bd1e92
Alex Babenko <wertuniul@gmail.com>
2023-03-03 22:32:34 +0300

    JavaHomework6/fix

--- src/test/java/StatsServiceTest.java
+++ src/test/java/StatsServiceTest.java
@@ -5,131 +5,125 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 import ru.netology.stats.StatsService;
 
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Objects;
 
 public class StatsServiceTest {
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/SumTestData.csv", numLinesToSkip=1)
-    public void sumSalesTest(int expected) {
+    @CsvFileSource(resources = "/SumTestData.csv", numLinesToSkip = 1)
+    public void sumSalesTest(int expected) throws IOException, URISyntaxException {
         String[] args;
-        try (CSVReader csvReader = new CSVReader(new FileReader("D:\\JavaHomework\\JavaHomework6\\src\\test\\resources\\SumTestData.csv"))) {
-            while ((args = csvReader.readNext()) != null) {
-                int[] sales = new int[args.length];
-                for (int i = 0; i < sales.length; i++) {
-                    sales[i] = Integer.parseInt(args[i]);
-                }
-                StatsService service = new StatsService();
-                int actual = service.sumSales(sales);
-                Assertions.assertEquals(expected, actual);
-                break;
+        File file = new File(Objects.requireNonNull(getClass().getResource("/SumTestData.csv")).toURI());
+        CSVReader csvReader = new CSVReader(new FileReader(file));
+        while ((args = csvReader.readNext()) != null) {
+            int[] sales = new int[args.length];
+            for (int i = 0; i < sales.length; i++) {
+                sales[i] = Integer.parseInt(args[i]);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            StatsService service = new StatsService();
+            int actual = service.sumSales(sales);
+            Assertions.assertEquals(expected, actual);
+            break;
         }
     }
 
+
     @ParameterizedTest
     @CsvFileSource(resources = "/AverageSumTestData.csv", numLinesToSkip = 1)
-    public void averageSumSalesTest(int expected) {
+    public void averageSumSalesTest(int expected) throws IOException, URISyntaxException {
         String[] args;
-        try (CSVReader csvReader = new CSVReader(new FileReader("D:\\JavaHomework\\JavaHomework6\\src\\test\\resources\\AverageSumTestData.csv"))) {
-            while ((args = csvReader.readNext()) != null) {
-                int[] sales = new int[args.length];
-                for (int i = 0; i < sales.length; i++) {
-                    sales[i] = Integer.parseInt(args[i]);
-                }
-                StatsService service = new StatsService();
-                int actual = service.averageSumSales(sales);
-                Assertions.assertEquals(expected, actual);
-                break;
+        File file = new File(Objects.requireNonNull(getClass().getResource("/AverageSumTestData.csv")).toURI());
+        CSVReader csvReader = new CSVReader(new FileReader(file));
+        while ((args = csvReader.readNext()) != null) {
+            int[] sales = new int[args.length];
+            for (int i = 0; i < sales.length; i++) {
+                sales[i] = Integer.parseInt(args[i]);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            StatsService service = new StatsService();
+            int actual = service.averageSumSales(sales);
+            Assertions.assertEquals(expected, actual);
+            break;
         }
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/MaxSalesTestData.csv", numLinesToSkip = 1)
-    public void maxSalesTest(int expected) {
+    public void maxSalesTest(int expected) throws IOException, URISyntaxException {
         String[] args;
-        try (CSVReader csvReader = new CSVReader(new FileReader("D:\\JavaHomework\\JavaHomework6\\src\\test\\resources\\MaxSalesTestData.csv"))) {
-            while ((args = csvReader.readNext()) != null) {
-                int[] sales = new int[args.length];
-                for (int i = 0; i < sales.length; i++) {
-                    sales[i] = Integer.parseInt(args[i]);
-                }
-                StatsService service = new StatsService();
-                int actual = service.maxSales(sales);
-                Assertions.assertEquals(expected, actual);
-                break;
+        File file = new File(Objects.requireNonNull(getClass().getResource("/MaxSalesTestData.csv")).toURI());
+        CSVReader csvReader = new CSVReader(new FileReader(file));
+        while ((args = csvReader.readNext()) != null) {
+            int[] sales = new int[args.length];
+            for (int i = 0; i < sales.length; i++) {
+                sales[i] = Integer.parseInt(args[i]);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            StatsService service = new StatsService();
+            int actual = service.maxSales(sales);
+            Assertions.assertEquals(expected, actual);
+            break;
         }
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/MinSalesTestData.csv", numLinesToSkip = 1)
-    public void minSalesTest(int expected) {
+    public void minSalesTest(int expected) throws IOException, URISyntaxException {
         String[] args;
-        try (CSVReader csvReader = new CSVReader(new FileReader("D:\\JavaHomework\\JavaHomework6\\src\\test\\resources\\MinSalesTestData.csv"))) {
-            while ((args = csvReader.readNext()) != null) {
-                int[] sales = new int[args.length];
-                for (int i = 0; i < sales.length; i++) {
-                    sales[i] = Integer.parseInt(args[i]);
-                }
-                StatsService service = new StatsService();
-                int actual = service.minSales(sales);
-                Assertions.assertEquals(expected, actual);
-                break;
+        File file = new File(Objects.requireNonNull(getClass().getResource("/MinSalesTestData.csv")).toURI());
+        CSVReader csvReader = new CSVReader(new FileReader(file));
+        while ((args = csvReader.readNext()) != null) {
+            int[] sales = new int[args.length];
+            for (int i = 0; i < sales.length; i++) {
+                sales[i] = Integer.parseInt(args[i]);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            StatsService service = new StatsService();
+            int actual = service.minSales(sales);
+            Assertions.assertEquals(expected, actual);
+            break;
         }
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/LowerAveSalesTestData.csv", numLinesToSkip = 1)
-    public void lowerAveSalesTest(int expected) {
+    public void lowerAveSalesTest(int expected) throws IOException, URISyntaxException {
         String[] args;
-        try (CSVReader csvReader = new CSVReader(new FileReader("D:\\JavaHomework\\JavaHomework6\\src\\test\\resources\\LowerAveSalesTestData.csv"))) {
-            while ((args = csvReader.readNext()) != null) {
-                int[] sales = new int[args.length];
-                for (int i = 0; i < sales.length; i++) {
-                    sales[i] = Integer.parseInt(args[i]);
-                }
-                StatsService service = new StatsService();
-                int actual = service.lowerAveSales(sales);
-                Assertions.assertEquals(expected, actual);
-                break;
+        File file = new File(Objects.requireNonNull(getClass().getResource("/LowerAveSalesTestData.csv")).toURI());
+        CSVReader csvReader = new CSVReader(new FileReader(file));
+        while ((args = csvReader.readNext()) != null) {
+            int[] sales = new int[args.length];
+            for (int i = 0; i < sales.length; i++) {
+                sales[i] = Integer.parseInt(args[i]);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            StatsService service = new StatsService();
+            int actual = service.lowerAveSales(sales);
+            Assertions.assertEquals(expected, actual);
+            break;
         }
     }
 
+
     @ParameterizedTest
     @CsvFileSource(resources = "/AboveAveSalesTestData.csv", numLinesToSkip = 1)
-    public void aboveAveSalesTest(int expected) {
+    public void aboveAveSalesTest(int expected) throws IOException, URISyntaxException {
         String[] args;
-        try (CSVReader csvReader = new CSVReader(new FileReader("D:\\JavaHomework\\JavaHomework6\\src\\test\\resources\\AboveAveSalesTestData.csv"))) {
-            while ((args = csvReader.readNext()) != null) {
-                int[] sales = new int[args.length];
-                for (int i = 0; i < sales.length; i++) {
-                    sales[i] = Integer.parseInt(args[i]);
-                }
-                StatsService service = new StatsService();
-                int actual = service.aboveAveSales(sales);
-                Assertions.assertEquals(expected, actual);
-                break;
+        File file = new File(Objects.requireNonNull(getClass().getResource("/AboveAveSalesTestData.csv")).toURI());
+        CSVReader csvReader = new CSVReader(new FileReader(file));
+        while ((args = csvReader.readNext()) != null) {
+            int[] sales = new int[args.length];
+            for (int i = 0; i < sales.length; i++) {
+                sales[i] = Integer.parseInt(args[i]);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            StatsService service = new StatsService();
+            int actual = service.aboveAveSales(sales);
+            Assertions.assertEquals(expected, actual);
+            break;
         }
     }
 }
+
 
 
 
